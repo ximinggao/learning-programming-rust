83f907c5317b9fd970e391c1814dcc06cc329091
Brook Xi <minggao.xi@gmail.com>
2020-03-04 22:49:01 +0800

    done.

--- binary-tree/src/lib.rs
+++ binary-tree/src/lib.rs
@@ -133,3 +133,111 @@ fn test_add_method_2() {
         vec!["Jupiter", "Mars", "Mercury", "Saturn", "Uranus", "Venus"]
     )
 }
+
+use self::BinaryTree::*;
+
+struct TreeIter<'a, T: 'a> {
+    unvisited: Vec<&'a TreeNode<T>>,
+}
+
+impl<'a, T: 'a> TreeIter<'a, T> {
+    fn push_left_edge(&mut self, mut tree: &'a BinaryTree<T>) {
+        while let NonEmpty(ref node) = *tree {
+            self.unvisited.push(node);
+            tree = &node.left;
+        }
+    }
+}
+
+impl<T> BinaryTree<T> {
+    fn iter(&self) -> TreeIter<T> {
+        let mut iter = TreeIter {
+            unvisited: Vec::new(),
+        };
+        iter.push_left_edge(self);
+        iter
+    }
+}
+
+impl<'a, T: 'a> IntoIterator for &'a BinaryTree<T> {
+    type Item = &'a T;
+    type IntoIter = TreeIter<'a, T>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<'a, T> Iterator for TreeIter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<&'a T> {
+        let node = match self.unvisited.pop() {
+            None => return None,
+            Some(n) => n,
+        };
+
+        self.push_left_edge(&node.right);
+
+        Some(&node.element)
+    }
+}
+
+#[test]
+fn external_iterator() {
+    fn make_node<T>(left: BinaryTree<T>, element: T, right: BinaryTree<T>) -> BinaryTree<T> {
+        NonEmpty(Box::new(TreeNode {
+            left,
+            element,
+            right,
+        }))
+    }
+
+    let subtree_l = make_node(Empty, "mecha", Empty);
+    let subtree_rl = make_node(Empty, "droid", Empty);
+    let subtree_r = make_node(subtree_rl, "robot", Empty);
+    let tree = make_node(subtree_l, "Jaeger", subtree_r);
+
+    let mut v = Vec::new();
+    for kind in &tree {
+        v.push(*kind);
+    }
+    assert_eq!(v, ["mecha", "Jaeger", "droid", "robot"]);
+
+    let left_subtree = make_node(Empty, "mecha", Empty);
+    let right_subtree = make_node(make_node(Empty, "droid", Empty), "robot", Empty);
+    let tree = make_node(left_subtree, "Jaeger", right_subtree);
+
+    let mut v = Vec::new();
+    let mut iter = TreeIter { unvisited: vec![] };
+    iter.push_left_edge(&tree);
+    for kind in iter {
+        v.push(*kind);
+    }
+    assert_eq!(v, ["mecha", "Jaeger", "droid", "robot"]);
+
+    let mut v = Vec::new();
+    for kind in &tree {
+        v.push(*kind);
+    }
+    assert_eq!(v, ["mecha", "Jaeger", "droid", "robot"]);
+
+    let mut v = Vec::new();
+    let mut state = tree.into_iter();
+    while let Some(kind) = state.next() {
+        v.push(*kind);
+    }
+    assert_eq!(v, ["mecha", "Jaeger", "droid", "robot"]);
+
+    assert_eq!(
+        tree.iter()
+            .map(|name| format!("mega-{}", name))
+            .collect::<Vec<_>>(),
+        vec!["mega-mecha", "mega-Jaeger", "mega-droid", "mega-robot"]
+    );
+
+    let mut iterator = tree.into_iter();
+    assert_eq!(iterator.next(), Some(&"mecha"));
+    assert_eq!(iterator.next(), Some(&"Jaeger"));
+    assert_eq!(iterator.next(), Some(&"droid"));
+    assert_eq!(iterator.next(), Some(&"robot"));
+    assert_eq!(iterator.next(), None);
+}
