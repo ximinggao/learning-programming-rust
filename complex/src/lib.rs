fde2ad80ae884221eea6f533c54e996ba0632bc3
Brook Xi <minggao.xi@gmail.com>
2020-06-16 22:35:39 +0800

    Add some code

--- complex/src/lib.rs
+++ complex/src/lib.rs
@@ -208,3 +208,52 @@ fn custom_display_impl() {
     assert_eq!(format!("{}", ninety), "0 + 2i");
     assert_eq!(format!("{:#}", ninety), "2 ∠ 90°");
 }
+
+struct Selector<T> {
+    elements: Vec<T>,
+    current: usize,
+}
+
+use std::ops::{Deref, DerefMut};
+
+impl<T> Deref for Selector<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.elements[self.current]
+    }
+}
+
+impl<T> DerefMut for Selector<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.elements[self.current]
+    }
+}
+
+#[test]
+fn test_selector() {
+    let mut s = Selector {
+        elements: vec!['x', 'y', 'z'],
+        current: 2,
+    };
+
+    assert_eq!(*s, 'z');
+    assert!(s.is_alphabetic());
+    *s = 'w';
+    assert_eq!(s.elements, ['x', 'y', 'w']);
+
+    fn show_it(thing: &str) {
+        println!("{}", thing);
+    }
+
+    use std::fmt::Display;
+    fn show_it_generic<T: Display>(thing: T){
+        println!("{}", thing);
+    }
+
+    let ss = Selector {
+        elements: vec!["good", "bad", "ugly"],
+        current: 0,
+    };
+    show_it(&ss);
+    show_it_generic(&ss as &str);
+}
